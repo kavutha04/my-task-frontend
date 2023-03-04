3e5f3be9feb474050a5bafd07e6a716fe61bad1b
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-04 11:46:07 +0300

    App functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
+import List from './List';
+import {useState, useEffect} from 'react';
 
 function App() {
+  let [taskData, setTaskData] = useState([])
+  let [refTask, setRefTask] = useState(true)
+  let [peopleData, setPeopleData] = useState([])
+  let [refPeople, setRefPeople] = useState(true)
+  let [categoryData, setCategoryData] = useState([])
+  let [refCategory, setRefCategory] = useState(true)
+
+  //Use Effects for rendering page
+
+  //Import tasks
+  useEffect(() => {
+    fetch("http://localhost:9292/tasks")
+      .then((res) => res.json())
+      .then((tasks) => {
+        if (refTask === true){
+          setTaskData(tasks)
+          setRefTask(false)
+        }
+      })
+  }, [refTask])
+/***************************** */
+
+//Import people
+  useEffect(() => {
+    fetch("http://localhost:9292/people")
+      .then((res) => res.json())
+      .then((people) => {
+        if (refPeople === true){
+          setPeopleData(people)
+          setRefPeople(false)
+        }
+      })
+  }, [refPeople])
+  /**************************** */
+
+  //Import categories
+  useEffect(() => {
+    fetch("http://localhost:9292/categories")
+      .then((res) => res.json())
+      .then(categories => {
+        if (refCategory === true){
+          setCategoryData(categories)
+          setRefCategory(false)
+        }
+      })
+  }, [refCategory])
+  /***************************** */
+
+  //Passed down as prop to AddToDo to update state after task creation
+  function handleAddTask(newTask){
+    const updatedTasks = [...taskData, newTask]
+    setTaskData(updatedTasks)
+    setRefTask(true)
+  }
+  /************************************************* */
+
+
+  //Passed down as prop to TaskCard to update state after task deletion
+  function handleDeleteTask(deletedTask){
+    const updatedTasks = taskData.filter((task) => task.id !== deletedTask)
+    setTaskData(updatedTasks)
+    setRefTask(true)
+  }
+/**************************************************** */
+
+//Passed down as prop to TaskCard to update after assigning person
+function handleUpdatePerson(){
+  setRefTask(true)
+}
+/********************************** */
+
+//Handles new person request
+function handleAddPerson(newPerson){
+  let newPeople = [...peopleData, newPerson]
+  setPeopleData(newPeople)
+  setRefPeople(true)
+}
+/*************************** */
+  
+//Handles deleting a person from list
+function handleDeletePerson(deletedPerson){
+  const newPeople = peopleData.filter((person) => person.id !== deletedPerson)
+  setPeopleData(newPeople)
+  setRefPeople(true)
+}
+/******************* */
+
+//Handles the sort functionality
+function handleSort(sortedTasks){
+  setTaskData(sortedTasks)
+}
+/**************************** */
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>What To Do...</h1>
+        <List tasks={taskData} categories={categoryData} people={peopleData} onSort={handleSort} assignPerson={handleUpdatePerson} onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} addPerson={handleAddPerson} deletePerson={handleDeletePerson} />
       </header>
     </div>
   );
